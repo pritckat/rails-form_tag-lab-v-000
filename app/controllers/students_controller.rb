69849d5f0a77096140d5e4e8179903cf7d1071f1
Katrina Pritchard <pritckat@gmail.com>
2018-09-22 03:24:36 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -7,6 +7,10 @@ class StudentsController < ApplicationController
   def new
   end
 
+  def create
+    Student.create(first_name: params[:first_name], last_name: params[:last_name])
+  end
+
   def show
     @student = Student.find(params[:id])
   end
